142c79d59ab0b7d926bd62f4ad1cc53773f473ff
pogo <borgster04@yahoo.com>
2023-11-02 17:45:08 +0200

    revamp

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -15,18 +15,22 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
 				uint256 maxDeposits;
 		}
 
-		WeightedToken[] weightedTokens;
+		WeightedToken[] public weightedTokens;
+		address[] public allTokens;
 
 		address public governor;
 		uint256 public globalTimeLock;
 
 		mapping(address user => mapping(address token => uint256 amount)) public deposits;
+		mapping(address user => uint256 oldVirtualBalance) public oldBalances;
 
-		event Deposit(address user, address token, uint depositAmount, uint govAmount);
+		event Deposit(address user, address token, uint depositAmount);
 		event Redeem(address user, address token, uint redeemAmount);
+		event Transfer(address indexed from, address indexed to, uint256 value);
 		event LockedUntil(uint256 timeToUnlock);
-		event TokenAdded(address token);
+		event TokenAdded(address token, uint256 weight, uint256 maxDeposits);
 		event TokenRemoved(address token);
+		event TokenUpdated(address token, uint256 weight, uint256 maxDeposits);
 
 		modifier notLocked(address from) {
 				require (from == address(0) || globalTimeLock < block.timestamp, "Mint/burn/transfer locked");
@@ -49,11 +53,9 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
 
 				deposits[msg.sender][token] += amount;
 
-				uint256 govTokens = tok.weight * amount;
+				updateBalance(msg.sender);
 
-				_mint(msg.sender, govTokens);
-
-				emit Deposit(msg.sender, token, amount, govTokens);
+				emit Deposit(msg.sender, token, amount);
 		}
 
 		function redeem(address token, uint256 amount) external notLocked(msg.sender) {
@@ -62,38 +64,66 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
 
 				deposits[msg.sender][token] -= amount;
 
-				(WeightedToken memory tok, ) = getWeightedToken(token);
-
-				uint256 govTokens = tok.weight * amount;
-
-				_burn(msg.sender, govTokens);
-
 				require(ERC20(token).transfer(msg.sender, amount),"Token transfer fail");
+
+				updateBalance(msg.sender);
 
 				emit Redeem(msg.sender, token, amount);
 		}
 
-		function addToken(address token, uint256 weight, uint256 maxDeposits) external onlyOwner {
-				(WeightedToken memory tok, uint256 idx) = getWeightedToken(token);
-				require(tok.maxDeposits == 0, "Already active");
+		function updateBalance(address user) internal {
+				uint256 newBalance = balanceOf(user);
+				uint256 oldBalance = oldBalances[user];
 
-				if (idx == type(uint).max){
-						weightedTokens.push(WeightedToken({token: token, weight: weight, maxDeposits: maxDeposits}));
+				if (newBalance > oldBalance){
+						emit Transfer(address(0), user, newBalance - oldBalance);
 				} else {
-						weightedTokens[idx] = WeightedToken({token: token, weight: weight, maxDeposits: maxDeposits});
+					if (oldBalance > newBalance){
+						emit Transfer(user, address(0), oldBalance- newBalance);
+					}
 				}
-				
 
-				emit TokenAdded(token);
+				oldBalances[user] = newBalance;
+		}
+
+		function addToken(address token, uint256 weight, uint256 maxDeposits) external onlyOwner {
+				(WeightedToken memory tok, ) = getWeightedToken(token);
+				require(tok.token == address(0), "Already exists");
+				require(tok.weight > 0, "Weight not zero");
+
+				weightedTokens.push(WeightedToken({token: token, weight: weight, maxDeposits: maxDeposits}));
+
+				// add to allTokens only once
+				bool exists;
+				for (uint i; i < allTokens.length ; i++){
+						if (tok.token == allTokens[i]) {
+								exists = true;
+								break;
+						}
+				}
+				if (!exists) allTokens.push(tok.token);
+				
+				emit TokenAdded(token, weight, maxDeposits);
 		}
 
 		function removeToken(address token) external onlyOwner {
 				(WeightedToken memory tok, uint256 idx) = getWeightedToken(token);
-				require(idx != type(uint).max && tok.maxDeposits > 0, "Already inactive");
+				require(tok.token != address(0), "Does not exist");
 
-				weightedTokens[idx] = WeightedToken({ token: tok.token, weight: tok.weight, maxDeposits: 0 });
+				weightedTokens[idx] = weightedTokens[ weightedTokens.length - 1 ];
+
+				weightedTokens.pop();
 
 				emit TokenRemoved(token);
+		}
+
+		function updateToken(address token, uint256 weight, uint256 maxDeposits) external onlyOwner {
+				(WeightedToken memory tok, uint256 idx) = getWeightedToken(token);
+				require(tok.token != address(0), "Does not exist");
+
+				weightedTokens[idx] = WeightedToken( {token: token, weight: weight, maxDeposits: maxDeposits} );
+
+				emit TokenUpdated(token, weight, maxDeposits);
 		}
 
 		function setGovernor(address newGovernor) external onlyOwner {
@@ -102,9 +132,13 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
 
 		function timeLock(uint256 timeToUnlock) external {
 				require(msg.sender == governor,"Not allowed");
-				globalTimeLock = timeToUnlock;
 
-				emit LockedUntil(timeToUnlock);
+				if (timeToUnlock > globalTimeLock) {
+						globalTimeLock = timeToUnlock;
+
+						emit LockedUntil(timeToUnlock);
+				}
+				
 		}
 
 		function getWeightedToken(address token) public view returns(WeightedToken memory, uint256 idx) {
@@ -120,15 +154,24 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
 
 		function balanceOf(address user) public view override returns (uint256 balance){
 				for (uint i	; i < weightedTokens.length ; i++) {
-						if (weightedTokens[i].maxDeposits > 0){
-								unchecked { balance += weightedTokens[i].weight * deposits[user][weightedTokens[i].token]; }
-						}
+						balance += weightedTokens[i].weight * deposits[user][weightedTokens[i].token];
 				}
+		}
+
+		function depositBalances(address user) public view returns(address[] memory, uint256[] memory) {
+				uint256[] memory balances = new uint256[](allTokens.length);
+
+				for (uint i; i < allTokens.length; i++){
+						balances[i] = deposits[user][allTokens[i]];
+				}
+
+				return (allTokens, balances);
 		}
 
     // The functions below are overrides required by Solidity.
 
     function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) notLocked(from) {
+				require(from == address(0) || to == address(0), "Transfers not supported");
         super._update(from, to, amount);
     }
 
